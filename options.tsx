cdb6f419833d9dffdec0b99dd9b4a717bdb9c11a
Mark Kretschmann <kretschmann@kde.org>
2022-10-05 19:48:23 +0200

    Add Storage for config

--- options.tsx
+++ options.tsx
@@ -1,16 +1,20 @@
-import { useState } from "react"
+// import { useState } from "react"
+import { useStorage } from "@plasmohq/storage/hook"
+
 
 function OptionsIndex() {
-  const [data, setData] = useState("")
+  // const [data, setData] = useState("")
+  const [key, setKey] = useStorage("openai_key")
 
   return (
     <div>
       <h1>
         Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
       </h1>
-      <h2>This is the Option UI page!</h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-    </div>
+      <h2>Enter OpenAI key:</h2>
+      {/* <input onChange={(e) => setData(e.target.value)} value={data} /> */}
+      <input onChange={(e) => setKey(e.target.value)} value={key} />
+      </div>
   )
 }
 
