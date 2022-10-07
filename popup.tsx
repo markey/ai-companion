64cc9ec3f0f16ae419411d1b3c4c3062514be3ad
Mark Kretschmann <kretschmann@kde.org>
2022-10-07 22:15:15 +0200

    Testing OpenAI-Node, not working because XHR

--- popup.tsx
+++ popup.tsx
@@ -1,8 +1,21 @@
+// OpenAI GPT-3 Prompt Generator Chrome extension
+
 import { useState } from "react"
+import { Storage } from "@plasmohq/storage"
+import { Configuration, OpenAIApi } from "openai";
+
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  console.log("Popup.tsx");
 
+  const [data, setData] = useState("")
+  const storage = new Storage();
+
+  const configuration = new Configuration({
+    apiKey: storage.get("openai_key"),
+  });
+  const openai = new OpenAIApi(configuration);
+  
   return (
     <div
       style={{
@@ -10,18 +23,23 @@ function IndexPopup() {
         flexDirection: "column",
         padding: 16
       }}>
+
       <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
+        Enter text to send to OpenAI:
       </h2>
+
       <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
-    </div>
+
+      <button onClick={ async () => { 
+        await openai.createCompletion({
+          model: "text-davinci-002",
+          prompt: data,
+        }).then((response) => { console.log("OpenAI response: ", response) })
+      }}>Send to OpenAI</button>
+
+      <button onClick={(e) => { console.log("CLICK") }}>Log to console</button>
+
+      </div>
   )
 }
 
