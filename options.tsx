3c1d7fae6fc803e636daee10357fe7f6bad1d9bf
Mark Kretschmann <kretschmann@kde.org>
2022-10-11 23:22:53 +0200

    autoformat

--- options.tsx
+++ options.tsx
@@ -14,7 +14,7 @@ function OptionsIndex() {
       <h2>Enter OpenAI key:</h2>
       {/* <input onChange={(e) => setData(e.target.value)} value={data} /> */}
       <input onChange={(e) => setKey(e.target.value)} value={key} />
-      </div>
+    </div>
   )
 }
 
