2385a12a322c42068c2789d982d752eb08fa6ecb
Mark Kretschmann <kretschmann@kde.org>
2022-10-13 15:33:57 +0200

    Port options page to MUI

--- options.tsx
+++ options.tsx
@@ -1,20 +1,27 @@
-// import { useState } from "react"
-import { useStorage } from "@plasmohq/storage/hook"
+import Button from "@mui/material/Button"
+import Divider from "@mui/material/Divider"
+import Input from "@mui/material/Input"
+import Stack from "@mui/material/Stack"
+import TextField from "@mui/material/TextField"
+import Typography from "@mui/material/Typography"
 
+import { useStorage } from "@plasmohq/storage/hook"
 
 function OptionsIndex() {
   // const [data, setData] = useState("")
   const [key, setKey] = useStorage("openai_key")
 
   return (
-    <div>
-      <h1>
-        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
-      </h1>
-      <h2>Enter OpenAI key:</h2>
-      {/* <input onChange={(e) => setData(e.target.value)} value={data} /> */}
-      <input onChange={(e) => setKey(e.target.value)} value={key} />
-    </div>
+    <Stack maxWidth={600} spacing={2}>
+      <Typography variant="h5">OpenAI Extension Options</Typography>
+
+      <TextField
+        label="Enter OpenAI key"
+        autoFocus
+        onChange={(e) => setKey(e.target.value)}
+        value={key}
+      />
+    </Stack>
   )
 }
 
